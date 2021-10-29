7950c007a5cb87858d243e832acc152069e36e12
Jonathan Long <jonathanelong@icloud.com>
2021-10-28 22:37:45 -0500

    Initial Commit

---
+++ Tests/MediaTests/MediaTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Media
+
+final class MediaTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Media().text, "Hello, World!")
+    }
+}
